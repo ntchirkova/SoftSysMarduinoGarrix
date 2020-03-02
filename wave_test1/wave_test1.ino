f92891fe6a2eeac355656e53421c8156fee57217
Nina Tchirkova <nina.tchirkova@students.olin.edu>
2020-03-02 09:56:29 -0500

    preliminary testing

---
+++ wave_test1/wave_test1.ino
@@ -0,0 +1,77 @@
+/*
+  
+ Author: Allen Downey 
+ 
+ Based on http://arduino.cc/en/Tutorial/AnalogInput
+ Created by David Cuartielles
+ modified 30 Aug 2011
+ By Tom Igoe
+ License: Public Domain
+ 
+ */
+ 
+ 
+int ledPin = 5;       // select the pin for the LED
+int buttonPin1 = 2;
+int buttonPin2 = 3;
+
+void setup() {
+  Serial.begin(9600);
+  
+  pinMode(buttonPin1, INPUT_PULLUP);  
+  pinMode(buttonPin2, INPUT_PULLUP);  
+
+  pinMode(ledPin, OUTPUT);
+  
+  pinMode(13, OUTPUT);  
+  pinMode(12, OUTPUT);  
+  pinMode(11, OUTPUT);  
+  pinMode(10, OUTPUT);  
+  pinMode(9, OUTPUT);  
+  pinMode(8, OUTPUT);  
+  pinMode(7, OUTPUT);  
+  pinMode(6, OUTPUT);  
+}
+
+void writeByte(int x) {
+  int pin;
+  
+  for (pin=13; pin>=6; pin--) {
+    digitalWrite(pin, x&1);
+    x >>= 1;
+  }
+}
+
+int low = 36;
+int high = 255;
+int stride = 5;
+int counter = low;
+byte sine[] = {127, 134, 142, 150, 158, 166, 173, 181, 188, 195, 201, 207, 213, 219, 224, 229, 234, 238, 241, 245, 247, 250, 251, 252, 253, 254, 253, 252, 251, 250, 247, 245, 241, 238, 234, 229, 224, 219, 213, 207, 201, 195, 188, 181, 173, 166, 158, 150, 142, 134, 127, 119, 111, 103, 95, 87, 80, 72, 65, 58, 52, 46, 40, 34, 29, 24, 19, 15, 12, 8, 6, 3, 2, 1, 0, 0, 0, 1, 2, 3, 6, 8, 12, 15, 19, 24, 29, 34, 40, 46, 52, 58, 65, 72, 80, 87, 95, 103, 111, 119,};
+int t = 0;
+size_t len = sizeof(sine)/sizeof(sine[0]);
+
+void loop() {
+  int button1 = digitalRead(buttonPin1);
+  int button2 = digitalRead(buttonPin2);
+  if (button2 && button1) return;
+
+  if (!button1) { 
+    counter += stride;
+    if (counter > high) {
+      counter = low;
+      //Serial.println(counter);
+    }
+    writeByte(counter);
+  }
+
+
+  if (!button2) {
+    if (t >= len) {
+      t = 0;
+    }
+    writeByte(sine[t]);
+    t++;
+  }
+
+  // write to the digital pins  
+}

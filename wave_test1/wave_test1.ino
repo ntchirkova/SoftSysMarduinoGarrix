eb1097cdee841aaea7f93bd522c6411b4c3bd388
Samantha Young <syoung@olin.edu>
2020-03-02 11:08:28 -0500

    adding frequency but has time lag issue

--- wave_test1/wave_test1.ino
+++ wave_test1/wave_test1.ino
@@ -42,12 +42,22 @@ void writeByte(int x) {
   }
 }
 
+int getFreqFromSlider(){
+  int in = 0;
+  int freq = 0;
+  in = analogRead(A0);
+  freq = map(in, 0, 1024, 150, 1200);
+  return freq;
+}
+
 int low = 36;
 int high = 255;
 int stride = 5;
 int counter = low;
 int sine_arr[] = {127, 134, 142, 150, 158, 166, 173, 181, 188, 195, 201, 207, 213, 219, 224, 229, 234, 238, 241, 245, 247, 250, 251, 252, 253, 254, 253, 252, 251, 250, 247, 245, 241, 238, 234, 229, 224, 219, 213, 207, 201, 195, 188, 181, 173, 166, 158, 150, 142, 134, 127, 119, 111, 103, 95, 87, 80, 72, 65, 58, 52, 46, 40, 34, 29, 24, 19, 15, 12, 8, 6, 3, 2, 1, 0, 0, 0, 1, 2, 3, 6, 8, 12, 15, 19, 24, 29, 34, 40, 46, 52, 58, 65, 72, 80, 87, 95, 103, 111, 119,};
 int t = 0;
+int frequency = 0;
+int in = 0;
 size_t len = sizeof(sine_arr)/sizeof(sine_arr[0]);
 
 int sine(int freq) {
@@ -58,8 +68,10 @@ int sine(int freq) {
 void loop() {
   int buttonSaw = digitalRead(buttonPin1);
   int buttonSine = digitalRead(buttonPin2);
+  frequency = getFreqFromSlider();
+  Serial.println(frequency); 
   if (buttonSine && buttonSaw) return;
-
+        
   if (!buttonSaw) { 
     counter += stride;
     if (counter > high) {
